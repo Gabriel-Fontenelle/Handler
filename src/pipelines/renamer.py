fc4f2e0731bb5f4c72e7f8d3a881a4ac7c4c5347
Gabriel <contato@gabrielfontenelle.com>
2020-09-23 00:00:02 -0300

    feat(pipelines/renamer): Add incomplete baseline class for renamers and complete inherent class for renamers of Linux, Windows and UUID4.

---
+++ src/pipelines/renamer.py
@@ -0,0 +1,137 @@
+# Python internals
+import re
+from uuid import uuid4
+
+# core modules
+from src.pipelines.__init__ import ProcessorMixin
+
+# modules
+from src.handler import FileSystem
+
+
+class Renamer(ProcessorMixin):
+    """
+    Base class to be inherent to define class to be used on Renamer pipelines.
+    """
+
+    file_system_handler = FileSystem
+    enumeration_pattern = None
+    
+    @classmethod
+    def prepare_filename(cls, filename, extension=None):
+        """
+        Method to separated extension from filename if extension
+        not informed and save on class.
+        """
+        # Remove extension from filename if it was given
+        if extension and filename[-len(extension):] == extension:
+            filename = filename[:-len(f".{extension}")]
+
+        return filename, extension
+
+    @classmethod
+    def get_name(cls, directory_path, filename, extension):
+        """
+        Method to get the new generated name.
+        """
+        raise NotImplementedError("Method get_name must be overwrite on child class.")
+
+    @classmethod
+    def process(cls, *args, **kwargs):
+        """
+        Method used to run this class on Processor`s Pipeline for Files.
+        This method and to_processor() is not need to compare files outside a pipelines.
+        This process method is created exclusively to pipeline for objects inherent from BaseFile.
+        """
+
+        # Prepare filename from File's object
+
+        # Get directory from object to be processed.
+
+        # Get new name
+
+        # Set new name at File's object
+
+        # Add old name to File's cache
+
+
+class WindowsRenamer(Renamer):
+    """
+    Class following Windows style of renaming existing file to be used on Renamer pipelines.
+    """
+    enumeration_pattern = re.compile(r' ?\([0-9]+\)$|\[[0-9]+\]$|$')
+
+    @classmethod
+    def get_name(cls, directory_path, filename, extension):
+        """
+        Method to get the new generated name.
+        If there is a duplicated name the new name will follow
+        the style of Windows: `new name (1).ext`
+        """
+        old_filename = filename
+
+        # Prepare filename and extension removing enumeration from filename
+        # and setting up a empty string is extension is None
+        filename = cls.enumeration_pattern.sub('', filename)
+        extension = f'.{extension}' if extension else ''
+
+        i = 0
+        while cls.file_system_handler.exists(directory_path + filename + extension):
+            i += 1
+            filename = cls.enumeration_pattern.sub(f' ({i})', filename)
+
+        return filename + extension
+
+
+class LinuxRenamer(Renamer):
+    """
+    Class following Linux style of renaming existing file to be used on Renamer pipelines.
+    """
+    enumeration_pattern = re.compile(r'( +)?\- +[0-9]+$|$')
+
+    @classmethod
+    def get_name(cls, directory_path, filename, extension):
+        """
+        Method to get the new generated name.
+        If there is a duplicated name the new name will follow
+        the style of Linux: `new name - 1.ext`
+        """
+        old_filename = filename
+
+        # Prepare filename and extension removing enumeration from filename
+        # and setting up a empty string is extension is None
+        filename = cls.enumeration_pattern.sub('', filename)
+        extension = f'.{extension}' if extension else ''
+
+        i = 0
+        while cls.file_system_handler.exists(directory_path + filename + extension):
+            i += 1
+            filename = cls.enumeration_pattern.sub(f' - {i}', filename)
+
+        return filename + extension
+
+
+class UniqueRenamer(Renamer):
+
+    @classmethod
+    def get_name(cls, directory_path, filename, extension):
+        """
+        Method to get the new generated name. The new name will be the UUID version 4.
+        This method will throw a BlockingIOError if there is more then
+        100 tries to generate a unique UUID4.
+        """
+        extension = f'.{extension}' if extension else ''
+
+        #Generate Unique filename
+        filename = uuid4()
+
+        i = 0
+        while cls.file_system_handler.exists(directory_path + filename + extension) and i < 100:
+            i += 1
+            #Generate Unique filename
+            filename = uuid4()
+
+        if i == 100:
+            raise BlockingIOError("Too many files being handler simultaneous")
+
+        return filename + extension
