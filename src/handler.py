d9aba7538f7970b23ff5d634f60b955e7b8e9c6a
Gabriel <contato@gabrielfontenelle.com>
2020-11-02 21:44:24 -0300

    feat(handler): Add class with methods specific for getting file ID at Linux and Windows.

--- src/handler.py
+++ src/handler.py
@@ -4,17 +4,21 @@ This file can be accessed directly.
 """
 
 # Python internals
+from datetime import datetime
+from filecmp import cmp
 from shutil import copyfile
 
 from os import (
     makedirs,
-    sep as os_sep, remove
+    popen,
+    sep as os_sep, remove, fsync,
+    stat
 )
 from os.path import (
     exists,
     isdir,
     getsize,
-    dirname
+    dirname, getmtime, getctime, join
 )
 from io import open
 
@@ -28,7 +32,8 @@ from psutil import (
 )
 
 __all__ = [
-    'FileSystem',
+    'WindowsFileSystem',
+    'LinuxFileSystem',
     'System',
 ]
 
@@ -68,6 +73,7 @@ class FileSystem:
         """
         Method to create directory in the file system.
         This method will try to create a directory only if it not exists already.
+        Override this method if that’s not appropriate for your storage.
         """
         # Create Directory
         if not directory_path:
@@ -84,6 +90,7 @@ class FileSystem:
         """
         Method to create a empty file.
         This method will try to create a file only if it not exists already.
+        Override this method if that’s not appropriate for your storage.
         """
         # Check if file exists.
         if cls.exists(file_path):
@@ -98,6 +105,27 @@ class FileSystem:
         return True
 
     @classmethod
+    def save_file(cls, file_path, content, **kwargs):
+        """
+        Method to save content on file.
+        This method will throw a exception if content is not iterable.
+        Override this method if that’s not appropriate for your storage.
+        """
+        content = iter(content)
+
+        if 'file_mode' not in kwargs:
+            kwargs['file_mode'] = 'a'
+
+        if 'write_mode' not in kwargs:
+            kwargs['write_mode'] = 'b'
+
+        with open(file_path, kwargs['file_mode'] + kwargs['write_mode']) as file_pointer:
+            for chunk in content:
+                file_pointer.write(chunk)
+                file_pointer.flush()
+                fsync(file_pointer.fileno())
+
+    @classmethod
     def copy(cls, file_path_origin, file_path_destination, force=False):
         """
         Method used to copy a file from origin to destination.
@@ -106,6 +134,7 @@ class FileSystem:
         produce a OSError exception if not enough space in destination.
 
         This method will overwrite destination file if force is True.
+        Override this method if that’s not appropriate for your storage.
         """
         if cls.exists(file_path_origin) and (not cls.exists(file_path_destination) or force):
             copyfile(file_path_origin, file_path_destination)
@@ -119,6 +148,7 @@ class FileSystem:
         Method used to move a file from origin to destination.
         This method do use copy_file to first copy the file and after send file to trash.
         The file only will be sent to trash if no exception was raised on copy.
+        Override this method if that’s not appropriate for your storage.
         """
         if cls.copy(file_path_origin, file_path_destination, force):
             cls.delete(file_path_origin)
@@ -133,6 +163,7 @@ class FileSystem:
         this method will delete permanently a file or directory
         specified in path if `force` is True. If `force` is False
         it will only send `path` to trash (It's more safe to send to trash).
+        Override this method if that’s not appropriate for your storage.
         """
         if not cls.exists(path):
             return False
@@ -155,6 +186,31 @@ class FileSystem:
         Override this method if that’s not appropriate for your storage.
         """
         return exists(path)
+
+    @classmethod
+    def cmp(cls, file_path_1, file_path_2):
+        """
+        Method used to compare file from file_path informed.
+        Override this method if that’s not appropriate for your storage.
+        """
+        # Compare data from pointers and not stats.
+
+        return cmp(file_path_1, file_path_2, False)
+
+    @classmethod
+    def join(cls, *paths):
+        """
+        Method used to concatenate two or more paths.
+        Override this method if that’s not appropriate for your storage.
+        """
+        return join(*paths)
+
+    @classmethod
+    def get_filename_from_path(cls, path):
+        """
+        Method used to get the filename from a complete path.
+        """
+        return path.basename(path)
 
     @classmethod
     def get_relative_path(cls, path, relative_to):
@@ -191,6 +247,97 @@ class FileSystem:
             return '../' * relative_to[last_bar:].count(cls.sep) + path[last_bar:]
 
         return path
+
+    @classmethod
+    def get_size(cls, path):
+        """
+        Method to get the size of file at path in bytes.
+        """
+        return getsize(path)
+
+    @classmethod
+    def get_modified_date(cls, path):
+        """
+        Method to get the modified time as datetime converted from float.
+        """
+        return datetime.fromtimestamp(getmtime(path))
+
+    @classmethod
+    def get_created_date(cls, path):
+        """
+        Try to get the date that a file was created, falling back to when it was
+        last modified if that isn't possible.
+        This method should be overwritten in child specific for Operational System.
+        """
+        raise NotImplementedError("Method get_created_date(path) should be accessed through inherent class.")
+
+    @classmethod
+    def get_path_id(cls, path):
+        """
+        Method to get the file system id for path.
+        This method should be overwritten in child specific for Operational System.
+        """
+        raise NotImplementedError("Method get_path_id(path) should be accessed through inherent class.")
+
+
+class WindowsFileSystem(FileSystem):
+    """
+    Class that standardized methods of file systems for Windows Operational System.
+    """
+
+    @classmethod
+    def get_path_id(cls, path):
+        """
+        Method to get the file system id for a path.
+        Path can be both a directory or file.
+        """
+        # TODO: Conclude function after testing on Windows.
+        file = r"C:\Users\Grandmaster\Desktop\testing.py"
+        output = popen(fr"fsutil file queryfileid {file}").read()
+
+    @classmethod
+    def get_created_date(cls, path):
+        """
+        Try to get the date that a file was created, falling back to when it was
+        last modified if that isn't possible.
+        See http://stackoverflow.com/a/39501288/1709587 for explanation.
+        Source: https://stackoverflow.com/a/39501288
+        """
+        time = getctime(path)
+
+        return datetime.fromtimestamp(time)
+
+
+class LinuxFileSystem(FileSystem):
+    """
+    Class that standardized methods of file systems for Linux Operational System.
+    """
+
+    @classmethod
+    def get_path_id(cls, path):
+        """
+        Method to get the file system id for a path.
+        Path can be both a directory or file.
+        """
+        return stat(path, follow_symlinks=False).st_ino
+
+    @classmethod
+    def get_created_date(cls, path):
+        """
+        Try to get the date that a file was created, falling back to when it was
+        last modified if that isn't possible.
+        See http://stackoverflow.com/a/39501288/1709587 for explanation.
+        Source: https://stackoverflow.com/a/39501288
+        """
+        stats = stat(path)
+        try:
+            time = stats.st_birthtime
+        except AttributeError:
+            # We're probably on Linux. No easy way to get creation dates here,
+            # so we'll settle for when its content was last modified.
+            time = stats.st_mtime
+
+        return datetime.fromtimestamp(time)
 
 
 class System:
