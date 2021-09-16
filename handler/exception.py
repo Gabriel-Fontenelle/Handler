fe817deefba4c00f1350abcd11c754070010d8cf
Gabriel <contato@gabrielfontenelle.com>
2021-09-16 13:41:34 -0300

    feat(exception): Add new exception for use in BaseFile.

--- handler/exception.py
+++ handler/exception.py
@@ -3,6 +3,7 @@ Module with custom exceptions in use at package.
 """
 
 __all__ = [
+	'ImproperlyConfiguredFile',
 	'NoInternalContentError'
 ]
 
@@ -11,4 +12,10 @@ class NoInternalContentError(Exception):
 	"""
 	Exception that defines errors for when no internal content is found in file.
 	Meaning that the file is not a container or compacted file.
+	"""
+
+
+class ImproperlyConfiguredFile(Exception):
+	"""
+	Exception that defines error for when a File was a missing configuration.
 	"""
