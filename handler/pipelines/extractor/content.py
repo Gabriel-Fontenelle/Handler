55b1e67974d1343563629f6ddc0db4a79b195309
Gabriel <contato@gabrielfontenelle.com>
2022-08-29 00:18:05 -0300

    feat(pipelines/extractor): Finish extractor for internal files of 7z.

--- handler/pipelines/extractor/content.py
+++ handler/pipelines/extractor/content.py
@@ -20,34 +20,83 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 Should there be a need for contact the electronic mail
 `handler <at> gabrielfontenelle.com` can be used.
 """
+from handler.pipelines.hasher import CRC32Hasher
 from tinytag import TinyTag
 
+from py7zr import SevenZipFile
+from py7zr.exceptions import Bad7zFile
+
 from .extractor import Extractor
+from ... import Pipeline
+
+from ...exception import ValidationError
 
 __all__ = [
     'AudioMetadataFromContentExtractor',
-    'CompressedFilesFromContentExtractor',
+    'ContentExtractor',
+    'SevenZipCompressedFilesFromContentExtractor',
+    'RARCompressedFilesFromContentExtractor',
+    'MimeTypeFromContentExtractor'
 ]
 
 
-class MastrokaFilesFromContentExtractor(Extractor):
+class ContentExtractor(Extractor):
+    """
+    Extractor class with focus to processing information from file's content.
+    This class was created to allow parsing and extraction of data designated to
+    internal files.
+    """
+
+    extensions = None
+    """
+    Attribute to store allowed extensions to be used in validator.
+    This attribute should be override in children classes.
+    """
     stopper = True
     """
     Variable that define if this class used as processor should stop the pipeline.
     """
 
+    @classmethod
+    def validate(cls, file_object):
+        """
+        Method to validate if content can be extract to given extension.
+        """
+        if cls.extensions is None:
+            raise NotImplementedError(f"The attribute extensions is not overwritten in child class {cls.__name__}")
 
-class PSDLayersFromContentExtractor(Extractor):
-    stopper = True
+        # The ValidationError should be captured in children classes else it will not register as an error and
+        # the pipeline will break.
+        if file_object.extension not in cls.extensions:
+            raise ValidationError(f"Extension {file_object.extension} not allowed in validate for class {cls.__name__}")
+
+
+class MastrokaFilesFromContentExtractor(ContentExtractor):
     """
-    Variable that define if this class used as processor should stop the pipeline.
+    Class to extract internal files from mka, mkv files.
     """
 
 
-class CompressedFilesFromContentExtractor(Extractor):
-    stopper = True
+class PSDLayersFromContentExtractor(ContentExtractor):
     """
-    Variable that define if this class used as processor should stop the pipeline.
+    Class to extract internal files from PSD files.
+    """
+
+
+class RARCompressedFilesFromContentExtractor(ContentExtractor):
+    """
+    Class to extract internal files from rar files.
+    """
+
+
+class SevenZipCompressedFilesFromContentExtractor(ContentExtractor):
+    """
+    Class to extract internal files from 7z files.
+    """
+
+    extensions = ['7z']
+    """
+    Attribute to store allowed extensions to be used in validator.
     """
 
     @classmethod
@@ -55,20 +104,63 @@ class CompressedFilesFromContentExtractor(Extractor):
         """
         Method to extract the information necessary from a file_object.
         """
-        pass
+        try:
+            cls.validate(file_object)
 
-        # Add internal file as File object to file. The content will be obtained from in memory file content.
-        hash_file = object_to_process.__class__(
-            path=f"{file_system.join(directory_path, hash_filename)}",
-            extract_data_pipeline=Pipeline(
-                'handler.pipelines.extracter.FilenameAndExtensionFromPathExtractor',
-                'handler.pipelines.extracter.MimeTypeFromFilenameExtractor',
-                'handler.pipelines.extracter.FileSystemDataExtractor'
-            ),
-            file_system_handler=file_system
-        )
+            # Reset buffer to initial location
+            if file_object._content.buffer.seekable():
+                file_object._content.buffer.seek(0)
 
-        file_object.meta.packed = True
+            file_system = file_object.file_system_handler
+            file_class = file_object.__class__
+
+            file7z = SevenZipFile(file=file_object._content.buffer)
+
+            for internal_file in file7z.list():
+                # Skip directories
+                if internal_file.is_directory:
+                    continue
+
+                # Skip duplicate only if not choosing to override.
+                if internal_file.filename in file_object._content and not overrider:
+                    continue
+
+                # Create file object for internal file
+                internal_file_object = file_class(
+                    path=internal_file.filename,
+                    extract_data_pipeline=Pipeline(
+                        'handler.pipelines.extracter.FilenameAndExtensionFromPathExtractor',
+                        'handler.pipelines.extracter.MimeTypeFromFilenameExtractor',
+                    ),
+                    file_system_handler=file_system
+                )
+
+                # Update creation and modified date
+                internal_file_object.create_date = internal_file.creationtime
+                internal_file_object.update_date = internal_file.creationtime
+
+                # Update size of file
+                internal_file_object.size = internal_file.uncompressed
+
+                # Update hash generating the hash file and adding its content
+                hash_file = CRC32Hasher.create_hash_file(
+                    object_to_process=file_object,
+                    digested_hex_value=internal_file.crc32
+                )
+                internal_file_object.hashes['crc32'] = internal_file.crc32, hash_file, CRC32Hasher
+
+                # Add internal file as File object to file.
+                file_object._content_files[internal_file.filename] = internal_file_object
+
+            # Reset buffer to initial location
+            if file_object._content.buffer.seekable():
+                file_object._content.buffer.seek(0)
+
+            file_object.meta.packed = True
+            return True
+
+        except (Bad7zFile, ValidationError):
+            return False
 
 
 class VideoMetadataFromContentExtractor(Extractor):
