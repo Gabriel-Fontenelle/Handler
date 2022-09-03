2b51671fcb4bfd5e525533dc09dd751210ab68d3
Gabriel <contato@gabrielfontenelle.com>
2022-09-03 19:56:26 -0300

    feat(serializer): Change serializer class to be used not from inherance.

--- handler/serializer.py
+++ handler/serializer.py
@@ -21,110 +21,199 @@ Should there be a need for contact the electronic mail
 `handler <at> gabrielfontenelle.com` can be used.
 """
 import inspect
-import json
-from datetime import datetime, time, date
+from io import IOBase
+from importlib import import_module
+
+from dill import dumps, loads, HIGHEST_PROTOCOL
+from json_tricks import loads as json_loads, dumps as json_dumps
 
 
 __all__ = [
-    'Serializer'
+    'PickleSerializer',
+    'JSONSerializer'
 ]
 
-from handler.exception import SerializerError
 
-
-class Serializer:
+class PickleSerializer:
     """
-    Class that allow handling of Serialization from object to JSON string and dictionary and Deserialization of JSON
-    string and dictionary to object.
-    TODO: Maybe allow in the future to Serializer be a parameter of BaseFile instead, allowing the user to pass its own
-     serializer or something with the same guise.
+    Class that allow handling of Serialization/Deserialization from object to pickle and from it to object.
     """
-
-    def _serialize_item(self, value, ignore_keys=[]):
-        """
-        Method to serialize individual values.
-        This method exists to recursive serialize list and dict objects.
-        """
-        if hasattr(value, 'to_dict'):
-            return value.to_dict(ignore_keys=ignore_keys)
-
-        elif isinstance(value, (str, int, bool, type(None))):
-            return value
-
-        elif isinstance(value, list):
-            return [self._serialize_item(key) for key in value]
-
-        elif isinstance(value, dict):
-            return {key: self._serialize_item(new_value) for key, new_value in value.items()}
-
-        elif isinstance(value, bytes):
-            return value.decode('utf-8')
-
-        elif isinstance(value, datetime):
-            return value.strftime("%m-%d-%Y %H:%M:%S")
-
-        elif isinstance(value, time):
-            return value.strftime("%H:%M:%S")
-
-        elif isinstance(value, date):
-            return value.strftime("%m-%d-%Y")
-
-        elif inspect.isclass(value):
-            return {'import_path': value.__module__, 'classname': value.__name__, 'object': False}
-
-        elif callable(value):
-            return {'import_path': value.__module__, 'classname': value.__name__, 'object': True}
-
-        else:
-            raise ValueError(f"Couldn't convert {value} to string in `_serialize_item` method!")
 
     @classmethod
-    def from_dict(cls, encoded_dict, **kwargs):
+    def serialize(cls, source):
         """
-        Method to deserialize a given dictionary to a instance of current child class.
-        This method must be overwritten in child class, as it is the responsibility of child class to implement how the
-        conversion from dictionary to object will play out.
+        Method to serialize the input `source` using dill as extension to `pickle`.
         """
-        raise NotImplementedError("This method `from_dict` should be overwrite in inherent classes!")
+        return dumps(source, protocol=HIGHEST_PROTOCOL, recurse=True)
 
-    def to_dict(self, ignore_keys=[], **kwargs):
+    @classmethod
+    def deserialize(cls, source):
         """
-        Method that serialize the current class object to a dictionary.
-        The parameter `ignore_keys` will be used to ignore attributes in a recursive way.
+        Method to deserialize the input `source` using dill as extension to `pickle`.
         """
-        encoded_dict = {}
+        return loads(source, protocol=HIGHEST_PROTOCOL, recurse=True)
 
-        for key, value in vars(self).items():
-            if key in ignore_keys:
+
+class JSONSerializer:
+    """
+    Class that allow handling of Serialization/Deserialization from object to json string and from it to object.
+    """
+
+    @classmethod
+    def _prepare_element(cls, source, cache=None):
+        """
+        Method to prepare the source converting it to a dict where class and objects are encoded as dictionaries to
+        allow serialization.
+
+        This method does not serialize class attributes for classes due to the fact that only the import is
+        serialized.
+        """
+        # Set up initial cache, parameter used for controlling the objects' reference for serializing __self__.
+        if cache is None:
+            cache = {}
+
+        object_id = id(source)
+        cache.append(object_id)
+
+        serialized_content = {}
+
+        for key, element in source.__serialize__.items():
+            if id(element) in cache:
+                # Serialize reference to previous encountered object.
+                serialized_content[key] = {"__self__": id(element)}
+
+            elif hasattr(element, "__serialize__"):
+                # Serialize supported object that has __serialize__ property.
+                serialized_content[key] = cls._prepare_element(source=element, cache=cache)
+
+            elif inspect.isclass(element):
+                # Serialize class
+                serialized_content[key] = {
+                    "__class__": "{module}.{name}".format(module=element.__module__, name=element.__name__)
+                }
+
+            elif isinstance(element, IOBase):
+                # Serialize buffer
+                serialized_content[key] = {
+                    "__buffer__": "{module}.{name}".format(module=element.__module__, name=element.__name__),
+                    "__class__":  "{module}.{name}".format(module=element.__module__, name=element.__name__)
+                }
+
+            else:
+                serialized_content[key] = element
+
+        return {
+            "__object__": "{module}.{name}".format(module=source.__class__.__module__, name=source.__class__.__name__),
+            "__attribute__": serialized_content,
+            "__id__": object_id
+        }
+
+    @classmethod
+    def _instantiate_element(cls, source, cache):
+        """
+        Method to prepare the source converting it to objects decoding the dictionaries in source to
+        allow deserialization.
+
+        This method does not deserialize class attributes for classes due to the fact that it is instantiated a new one.
+        """
+        deserialized_content = {}
+
+        # Get class and module for object
+        modulename, classname = source["__object__"].rsplit(".", 1)
+
+        # Load class
+        class_instance = getattr(import_module(modulename), classname)
+
+        for key, element in source["__attribute__"].items():
+            if isinstance(element, dict):
+                if "__object__" in element:
+                    # Convert element to object
+                    deserialized_content[key] = cls._instantiate_element(source=element, cache=cache)
+
+                elif "__buffer__" in element:
+                    # Convert element to buffer
+                    pass
+
+                elif "__class__" in element:
+                    # Convert element to class
+                    modulename, classname = element["__class__"].rsplit(".", 1)
+
+                    # Import class
+                    deserialized_content[key] = getattr(import_module(modulename), classname)
+
+                else:
+                    deserialized_content[key] = element
+            else:
+                deserialized_content[key] = element
+
+        # Instantiate the current object
+        instance = class_instance(**deserialized_content)
+
+        # Register reference in cache
+        cache[source["__id__"]] = instance
+
+        return instance
+
+    @classmethod
+    def _fix_self_reference(cls, source, cache):
+        """
+        Method to fix the references present in instance source that were not able to be converted in
+        _instantiate_element to allow deserialization to finish.
+        """
+        # Add the instance id to cache of inspections concluded to avoid calling fix_self_reference
+        # again for object already inspect.
+        cache["done"].append(id(source))
+
+        for attribute, value in inspect.getmembers(source):
+            # Ignore special attributes
+            if attribute[0:2] == "__" and attribute[-2:] == "__":
                 continue
 
-            try:
-                encoded_dict[key] = self._serialize_item(
-                    value=value,
-                    ignore_keys=ignore_keys
-                )
-            except ValueError:
-                raise SerializerError(f"Couldn't convert {value} to string for use in `to_dict` method!")
+            # Ignore non object
+            elif (
+                    inspect.isclass(value)
+                    or inspect.ismethod(value)
+                    or inspect.isdatadescriptor(value)
+                    or inspect.isfunction(value)
+                    or inspect.ismemberdescriptor(value)
+                    or inspect.isgenerator(value)
+            ):
+                continue
 
-        return encoded_dict
+            if isinstance(value, dict) and "__self__" in value:
+                setattr(source, attribute, cache[value["__self__"]])
 
-    def to_json(self, ignore_keys=[], **kwargs):
-        """
-        Method that serialize the current class object to JSON string.
-        The parameter `ignore_keys` will be used to ignore attributes in a recursive way.
-        """
-        return json.dumps(
-            self.to_dict(ignore_keys=ignore_keys, **kwargs)
-        )
-
-    for_json = to_json
-    """
-    Alias that allow simplejson to work when option for_json is set as True.
-    """
+            elif hasattr(value, "__serialize__") and not callable(value) and id(value) not in cache["done"]:
+                cls._fix_self_reference(value, cache)
 
     @classmethod
-    def from_json(cls, value, **kwargs):
+    def serialize(cls, source):
         """
-        Method to deserialize a given JSON string to a instance of current child class.
+        Method to serialize the input `source` using json_tricks as extension to `json`.
         """
-        return cls.from_dict(json.loads(value), **kwargs)
+        # Prepare content from __serialize__ with new dictionary for the cache
+        serialized_content = cls._prepare_element(source, cache=[])
+
+        # Convert to JSON.
+        return json_dumps(serialized_content)
+
+    @classmethod
+    def deserialize(cls, source):
+        """
+        Method to deserialize the input `source` using json_tricks as extension to `json`.
+        """
+        # Prepare content to be parsed
+        dictionary_content = json_loads(source, preserve_order=False)
+
+        # Create cache dictionary to fix __self__ reference. The cache will be a
+        # dictionary with pair id: instance and one special pair string: list for controlling
+        # recursion.
+        cache = {"done": []}
+
+        # Convert elements of dictionary to objects
+        instance = cls._instantiate_element(source=dictionary_content, cache=cache)
+
+        # Fix self reference. This need to be done after creating the instances.
+        cls._fix_self_reference(source=instance, cache=cache)
+
+        return instance
