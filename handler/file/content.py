37fd6ee6c660a2d0a8f56299941bf51a3326acd4
Gabriel <contato@gabrielfontenelle.com>
2022-09-07 15:18:16 -0300

    refactor(file/content): Create method reset to reset buffer if seekable.

--- handler/file/content.py
+++ handler/file/content.py
@@ -166,8 +166,7 @@ class FileContent:
                     self.cached = True
 
             # Reset buffer to begin from first position
-            if self.buffer.seekable():
-                self.buffer.seek(0)
+            self.reset()
 
             raise StopIteration()
 
@@ -283,10 +282,16 @@ class FileContent:
             # Should not reach here if object is not seekable, but
             # to avoid problems with override of `should_load_to_memory` property
             # we check before using seek to reset the content.
-            if self.buffer.seekable():
-                self.buffer.seek(0)
+            self.reset()
 
             return self.buffer
+
+    def reset(self):
+        """
+        Method to reset the content cached or buffer if allowed.
+        """
+        if self.buffer.seekable():
+            self.buffer.seek(0)
 
 
 class FilePacket:
