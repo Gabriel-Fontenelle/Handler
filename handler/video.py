e0475c4a14aae9132f3b1c57aa1b17603740880e
Gabriel <contato@gabrielfontenelle.com>
2022-10-07 23:44:42 -0300

    feat(handler): Add method to VideEngine class.

--- handler/video.py
+++ handler/video.py
@@ -26,9 +26,14 @@ class VideoEngine:
     """
     Class that standardized methods of different video manipulators.
     """
+
     video = None
     """
     Attribute where the current video converted from buffer is stored.
+    """
+    metadata = None
+    """
+    Attribute where the current video metadata is stored.
     """
 
     def __init__(self, buffer):
@@ -40,6 +45,47 @@ class VideoEngine:
 
         self.prepare_video()
 
+    def get_duration(self):
+        """
+        Method to return the duration in seconds of the video.
+        This method should be overwritten in child class.
+        """
+        raise NotImplementedError("The method get_duration should be override in child class.")
+
+    def get_frame_rate(self):
+        """
+        Method to return the framerate of the video.
+        This method should be overwritten in child class.
+        """
+        raise NotImplementedError("The method get_frame_rate should be override in child class.")
+
+    def get_frame_amount(self):
+        """
+        Method to return the total amount of frame available in the video.
+        """
+        return int(self.get_duration() * self.get_frame_rate())
+
+    def get_frame_as_bytes(self, index, encode_format="jpeg"):
+        """
+        Method to return content of the frame at index as bytes.
+        This method should be overwritten in child class.
+        """
+        raise NotImplementedError("The method get_frame_as_bytes should be override in child class.")
+
+    def get_frame_image(self, index):
+        """
+        Method to return the array representing the frame at index.
+        This method should be overwritten in child class.
+        """
+        raise NotImplementedError("The method get_frame_image should be override in child class.")
+
+    def get_size(self):
+        """
+        Method to return the width and height of the video.
+        This method should be overwritten in child class.
+        """
+        raise NotImplementedError("The method get_size should be override in child class.")
+
     def prepare_video(self):
         """
         Method to prepare the video using the stored buffer as the source.
@@ -48,3 +94,19 @@ class VideoEngine:
         """
         raise NotImplementedError("The method prepare_video should be override in child class.")
 
+    def show(self):
+        """
+        Method to display the video for debugging purposes.
+        """
+        total_frames = self.get_frame_amount()
+
+        frame = 0
+
+        while frame < total_frames:
+            cv2.imshow("Video", self.get_frame_image(frame))
+            frame += 1
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        cv2.destroyAllWindows()
