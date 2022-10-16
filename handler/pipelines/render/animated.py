eb77c12335fc996831a6e63f8c5017d824e60964
Gabriel <contato@gabrielfontenelle.com>
2022-10-15 21:42:54 -0300

    feat(pipelines/render): Add class to render animated PSD, documents and video.

--- handler/pipelines/render/animated.py
+++ handler/pipelines/render/animated.py
@@ -20,14 +20,21 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 Should there be a need for contact the electronic mail
 `handler <at> gabrielfontenelle.com` can be used.
 """
+from io import BytesIO
 
-from .. import Pipeline
+import fitz
+from psd_tools import PSDImage
+
 from .static import StaticRender
+from .. import Pipeline
 
 __all__ = [
     'AnimatedRender',
+    'DocumentAnimatedRender',
     'ImageAnimatedRender',
+    'PSDAnimatedRender',
     'StaticAnimatedRender',
+    'VideoAnimatedRender',
 ]
 
 
@@ -99,7 +106,8 @@ class StaticAnimatedRender(AnimatedRender):
 
 class ImageAnimatedRender(AnimatedRender):
     """
-    Render class for processing information from file's content focusing in rendering the whole image.
+    Render class for processing information from file's content focusing in rendering a sample of the whole animated
+    image.
     This class make use of sequences.
     """
 
@@ -125,6 +133,174 @@ class ImageAnimatedRender(AnimatedRender):
         image.resize(defaults.width, defaults.height, keep_ratio=defaults.keep_ratio)
 
         # Set animated file for current file_object.
+        file_object._thumbnail._animated_file = cls.create_file(
+            file_object,
+            content=image.get_buffer(encode_format=defaults.format)
+        )
+
+
+class DocumentAnimatedRender(AnimatedRender):
+    """
+    Render class for processing information from file's content focusing in rendering a sample of the document's pages.
+    This class make use of sequences.
+    """
+
+    extensions = ["pdf", "epub", "fb2", "xps", "oxps"]
+    """
+    Attribute to store allowed extensions for use in `validator`.
+    """
+
+    @classmethod
+    def render(cls, file_object, **kwargs: dict):
+        """
+        Method to render the animated representation of the file_object that has pages.
+        """
+        image_engine = kwargs.pop('image_engine')
+
+        defaults = file_object._thumbnail.animated_defaults
+
+        # Use fitz from PyMuPDF to open the document.
+        # Because BufferedReader (default return for file_system.open) is not accept
+        # we need to consume to get its bytes as bytes are accepted as stream.
+        doc = fitz.open(
+            stream=file_object.buffer.read(),
+            filetype=file_object.extension,
+            # width and height are only used for content that requires rendering of vectors as `epub`.
+            width=defaults.width * 5,
+            height=defaults.height * 5
+        )
+
+        images = []
+
+        total_frames = doc.page_count
+        steps = total_frames // (total_frames * defaults.duration // 100)
+
+        for page in doc.pages(0, total_frames, steps):
+            bitmap = page.get_pixmap(dpi=defaults.format_dpi)
+
+            # Save the image in buffer with Pillow.
+            buffer = BytesIO()
+            bitmap.pil_save(fp=buffer, format=defaults.format)
+            buffer.seek(0)
+
+            image = image_engine(buffer=buffer)
+
+            # Trim white space originated from epub.
+            image.trim(color=defaults.color_to_trim)
+
+            # Resize image using the image_engine and default values.
+            image.resize(defaults.width, defaults.height, keep_ratio=defaults.keep_ratio)
+
+            # Append buffers
+            images.append(image)
+
+        # Create sequence image.
+        image = images[0].clone()
+        image.append_to_sequence(images=images[1:])
+
+        # Set static file for current file_object.
+        file_object._thumbnail._animated_file = cls.create_file(
+            file_object,
+            content=image.get_buffer(encode_format=defaults.format)
+        )
+
+
+class PSDAnimatedRender(AnimatedRender):
+    """
+    Render class for processing information from file's content focusing in rendering a sample of the PSD layers.
+    This class make use of sequences.
+    """
+
+    extensions = ["psd", "psb"]
+    """
+    Attribute to store allowed extensions for use in `validator`.
+    """
+
+    @classmethod
+    def render(cls, file_object, **kwargs: dict):
+        """
+        Method to render the animated representation of the file_object that has layers.
+        """
+        image_engine = kwargs.pop('image_engine')
+
+        defaults = file_object._thumbnail.static_defaults
+
+        # Load PSD from buffer
+        psd = PSDImage.open(fp=file_object.buffer)
+
+        images = []
+
+        total_frames = len(psd.layers)
+        steps = total_frames // (total_frames * defaults.duration // 100)
+
+        # Compose image from PSD visible layers and
+        # convert it to RGB to remove alpha channel before saving it to buffer.
+        for index in set(range(0, total_frames, steps)):
+            buffer = BytesIO()
+            psd.layers[index].composite().convert(mode="RGB").save(fp=buffer, format=defaults.format)
+
+            # Reset buffer to beginning before being loaded in image_engine.
+            buffer.seek(0)
+
+            image = image_engine(buffer=buffer)
+
+            image.resize(defaults.width, defaults.height, keep_ratio=defaults.keep_ratio)
+
+            # Append buffers
+            images.append(image)
+
+        # Create sequence image.
+        image = images[0].clone()
+        image.append_to_sequence(images=images[1:])
+
+        # Set static file for current file_object.
+        file_object._thumbnail._animated_file = cls.create_file(
+            file_object,
+            content=image.get_buffer(encode_format=defaults.format)
+        )
+
+
+class VideoAnimatedRender(AnimatedRender):
+    """
+    Render class for processing information from file's content focusing in rendering a sample of the video frames.
+    This class make use of sequences.
+    """
+
+    extensions = ["avi", "mkv", "mpg", "mpeg", "mp4", "flv"]
+    """
+    Attribute to store allowed extensions for use in `validator`.
+    """
+
+    @classmethod
+    def render(cls, file_object, **kwargs: dict):
+        """
+        Method to render the animated representation of the file_object that has frames.
+        """
+        image_engine = kwargs.pop('image_engine')
+        video_engine = kwargs.pop('video_engine')
+
+        defaults = file_object._thumbnail.static_defaults
+
+        video = video_engine(buffer=file_object.buffer)
+
+        total_frames = video.get_frame_amount()
+        steps = total_frames // (total_frames * defaults.duration // 100)
+
+        images = []
+
+        for index in set(range(0, total_frames, steps)):
+            image = image_engine(buffer=BytesIO(video.get_frame_as_bytes(index=index, encode_format=defaults.format)))
+
+            image.resize(defaults.width, defaults.height, keep_ratio=defaults.keep_ratio)
+
+            # Append buffers
+            images.append(image)
+
+        # Create sequence image.
+        image = images[0].clone()
+        image.append_to_sequence(images=images[1:])
+
+        # Set static file for current file_object.
         file_object._thumbnail._animated_file = cls.create_file(
             file_object,
             content=image.get_buffer(encode_format=defaults.format)
